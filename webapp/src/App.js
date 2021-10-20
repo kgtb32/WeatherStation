e34bd96a1178e22d3bf3c2e174ff6a606fd88281
basile <basile-lequeux@hotmail.fr>
2021-10-20 14:14:04 +0200

    setup react-app

---
+++ webapp/src/App.js
@@ -0,0 +1,22 @@
+import cloudSun from './logo_weather/cloud-sun.svg'
+import cloudRain from './logo_weather/cloud-rain.svg'
+import cloud from './logo_weather/cloud.svg'
+import sun from './logo_weather/sun.svg'
+import thermometer from './logo_weather/thermometer-50.svg'
+import rain from './logo_weather/rain.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="Logo_grid">
+        <img src={cloud} className="App-logo--no-anim" alt="logo" />
+        <img src={cloudSun} className="App-logo--no-anim" alt="logo" />
+        <img src={cloudRain} className="App-logo--no-anim" alt="logo" />
+        <img src={sun} className="App-logo--no-anim" alt="logo" />
+        <img src={thermometer} className="App-logo--no-anim" alt="logo" />
+        <img src={rain} className="App-logo--no-anim" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
