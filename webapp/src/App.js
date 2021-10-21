0b632d61ec4791bd9f924225ad316ae21bab19da
Jajoute <invictusrokenson@gmail.com>
2021-10-21 14:33:44 +0200

    Firebase initialize

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,8 +7,36 @@ import sun from './logo_weather/sun.svg'
 import thermometer from './logo_weather/thermometer-50.svg'
 import rain from './logo_weather/rain.svg'
 import './App.css';
+import React,{useEffect, useState} from 'react';
+import app from './config/firebase.js';
+import { getFirestore, collection, query, where, onSnapshot } from 'firebase/firestore';
+import meteo from './model/meteo_class.js';
 
 function App() {
+  const [meteo, setMeteo] = useState([{humidity:"", pression:"", temperature:""}]);
+  
+  const fetchMeteo=async()=>{
+    const db = getFirestore(app);
+    // const meteoCollection = collection(db, 'meteo');
+    // const meteoSnapshot = await getDocs(meteoCollection);
+    // const meteoList = meteoSnapshot.docs.map(doc => doc.data());
+    // console.log(meteoList.map(e => e))
+    
+    const q = query(collection(db, "meteo"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        //console.log("doc",doc)
+        setMeteo([...meteo,doc.data()])
+        console.log("doc.data",doc.data())
+      });
+
+    });
+  }
+
+  useEffect(() => {
+    fetchMeteo();
+  }, [])
+
   return (
     <div className="Logo_grid">
         <img src={cloud} className="App-logo" alt="logo" />
@@ -17,6 +45,7 @@ function App() {
         <img src={sun} className="App-logo" alt="logo" />
         <img src={thermometer} className="App-logo" alt="logo" />
         <img src={rain} className="App-logo" alt="logo" />
+        <p>{meteo.map((doc)=>doc.humidity)}</p>
     </div>
   );
 }
