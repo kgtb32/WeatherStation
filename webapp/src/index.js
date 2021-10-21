84dc6349315234ce6cda0261eddd11dda8b61b4a
basile <basile-lequeux@hotmail.fr>
2021-10-21 11:37:18 +0200

    Webapp : sidebar + navigation

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Sidebar from './components/SideBar';
+import temperatures from './pages/Temperatures'
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
       <div className="container_flex">
         <Sidebar />
-        <App />
+        <Switch>
+          <Route path="/" component={App} exact/>
+          <Route path="/temperatures" component={temperatures} exact/>
+        </Switch>
       </div>
     </React.StrictMode>
-    </BrowserRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
