d5d6cf0f7f6d97b020c78a3915b0ddc48e228597
basile <basile-lequeux@hotmail.fr>
2021-10-21 15:00:24 +0200

    Fix real time firebase

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import { getFirestore, collection, query, where, onSnapshot } from 'firebase/firestore';
+
+import meteo from './model/meteo_class.js';
+import app from './config/firebase.js';
+
 
 import cloudSun from './logo_weather/cloud-sun.svg'
 import cloudRain from './logo_weather/cloud-rain.svg'
@@ -7,15 +12,11 @@ import sun from './logo_weather/sun.svg'
 import thermometer from './logo_weather/thermometer-50.svg'
 import rain from './logo_weather/rain.svg'
 import './App.css';
-import React,{useEffect, useState} from 'react';
-import app from './config/firebase.js';
-import { getFirestore, collection, query, where, onSnapshot } from 'firebase/firestore';
-import meteo from './model/meteo_class.js';
 
 function App() {
   const [meteo, setMeteo] = useState([{humidity:"", pression:"", temperature:""}]);
   
-  const fetchMeteo=async()=>{
+  const fetchMeteo =async()=> {
     const db = getFirestore(app);
     // const meteoCollection = collection(db, 'meteo');
     // const meteoSnapshot = await getDocs(meteoCollection);
@@ -24,15 +25,21 @@ function App() {
     
     const q = query(collection(db, "meteo"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let newState = []
       querySnapshot.forEach((doc) => {
-        //console.log("doc",doc)
-        setMeteo([...meteo,doc.data()])
-        console.log("doc.data",doc.data())
+        const value = doc.data()
+        newState.push({
+          humidity: value.humidity,
+          pression: value.pression,
+          temperature: value.temperature
+        })
       });
+      setMeteo(newState)
 
     });
   }
 
+  console.log("meteo", meteo)
   useEffect(() => {
     fetchMeteo();
   }, [])
@@ -45,7 +52,9 @@ function App() {
         <img src={sun} className="App-logo" alt="logo" />
         <img src={thermometer} className="App-logo" alt="logo" />
         <img src={rain} className="App-logo" alt="logo" />
-        <p>{meteo.map((doc)=>doc.humidity)}</p>
+        {meteo.map(e =>
+          <div className="data_label">{e.temperature}</div>
+          )}
     </div>
   );
 }
