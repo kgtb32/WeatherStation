758bd7a02d3f8a0a9daf287fa7a70fe1df91374b
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-10-22 17:13:20 +0200

    Add i18n in Home Page

--- webapp/src/pages/Home.jsx
+++ webapp/src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
+import { useTranslation } from 'react-i18next';
+
 import TopBar from '../components/TopBar/TopBar'
 import ChartData from '../components/ChartData/ChartData'
+import i18n from "../i18n";
 
 function Home(){
+
+    const { t } = useTranslation()
+    
     return (
         <div className="w-100">
             <TopBar />
             <h1>Weather Station</h1>
+            <p onClick={() => {
+                i18n.changeLanguage('en')
+            }}>{t('langName')}</p>
             <ChartData />
         </div>
     )
