3d7f6196f748279156bc2ec42a582c19aba9396f
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-10-22 12:12:54 +0200

    Fixed lastUpdate bug

--- raspberry/firebaseHelper.py
+++ raspberry/firebaseHelper.py
@@ -3,17 +3,15 @@ import json
 import os
 import subprocess
 
-lastUpdate = 0
-
-def upload_to_firebase(pressure, humidity, temperature):
+def upload_to_firebase(pressure, humidity, temperature, lastUpdate):
     dateEpoch = subprocess.run(["date","-u","+%s"], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','')
-    dateHour = subprocess.run(["date","-u", "+H"], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','').
+    dateHour = subprocess.run(["date","-u", "+%H"], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','')
     shouldValBeUsed = False
 
     vals = [[0,6], [1,12],[2,18],[3,23]]
 
     for val in vals:
-        if(int(dateHour) == val[1] and lastUpdate == vals[0]):
+        if(int(dateHour) >= val[1] and (lastUpdate == val[0] or lastUpdate < val[0])):
             lastUpdate += 1
             shouldValBeUsed = True
             break
@@ -29,3 +27,4 @@ def upload_to_firebase(pressure, humidity, temperature):
     }
 
     os.system("/home/pi/WeatherStation/raspberry/fuego --credentials /home/pi/WeatherStation/raspberry/credentials.json add meteo '%s'" % json.dumps(meteoData).__str__())
+    return lastUpdate
