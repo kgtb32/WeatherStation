7febb4b9c7f8070f3f97a0792c4e75e867a015d8
Jajoute <invictusrokenson@gmail.com>
2021-10-22 17:27:09 +0200

    construction sidebar

--- webapp/src/components/SideBar/SideBar.js
+++ webapp/src/components/SideBar/SideBar.js
@@ -1,51 +1,64 @@
-import React, { useState } from 'react';
-import * as IoIcons from 'react-icons/io';
-import './SideBar.css';
-import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import { Link } from 'react-router-dom';
-import thermometer from '../../logo_weather/thermometer-50.svg'
-import raindrop from '../../logo_weather/raindrop.svg'
+import React from "react";
+import { Link } from "react-router-dom";
 
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarContent,
+  SidebarFooter
+} from "react-pro-sidebar";
+
+import * as IoIcons from "react-icons/io";
+import {RiTempColdFill } from 'react-icons/ri'
+
+import "react-pro-sidebar/dist/css/styles.css";
 
 export default function Sidebar() {
-    return (
-        <div className="sidebar">
-            <ProSidebar>
-                <SidebarHeader>
-                    {<>
-                        <span> Weather Webapp</span>
-                    </>
-                    }
-                </SidebarHeader>
-                <Menu iconShape="square">
-                    <MenuItem>
-                        Acceuil
-                        <Link to="/" />
-                    </MenuItem>
-                    <SubMenu title="Options">
-                        <MenuItem
-                        icon={<img src={thermometer} className="logo_sidebar" alt="logo" />}>
-                            Températures
-                            <Link to="/temperatures" />
-                        </MenuItem>
-                        <MenuItem 
-                        icon={<img src={raindrop} className="logo_sidebar" alt="logo" />}>
-                            Taux d'humidité
-                        <Link to="/humidity" />
-                        </MenuItem>
-                        <MenuItem 
-                        icon={<img src={raindrop} className="logo_sidebar" alt="logo" />}>
-                            Pression Atmosphérique
-                        <Link to="/pressure" />
-                        </MenuItem>
-                    </SubMenu>
-                    <MenuItem icon={<IoIcons.IoMdSettings />}>
-                        Paramètres
-                        <Link to="/settings" />
-                    </MenuItem>
-                </Menu>
-            </ProSidebar>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <ProSidebar
+      rtl={false}
+      collapsed={false}
+      toggled={false}
+      breakPoint="md"
+      >
+        <SidebarHeader>
+            <h4 className="text-center">Weather Station</h4>
+        </SidebarHeader>
+        <SidebarContent>
+        <Menu iconShape="circle" >
+          <MenuItem icon={<IoIcons.IoMdHome />}>
+            Acceuil
+            <Link to="/" />
+          </MenuItem>
+          <SubMenu title="Historique" icon={<IoIcons.IoMdTime />}>
+            <MenuItem icon={<RiTempColdFill />}>
+              Températures
+              <Link to="/temperatures" />
+            </MenuItem>
+            <MenuItem icon={<IoIcons.IoIosWater />}>
+              Taux d'humidité
+              <Link to="/humidity" />
+            </MenuItem>
+            <MenuItem icon={<IoIcons.IoIosAnalytics />}>
+              Pression Atmosphérique
+              <Link to="/pressure" />
+            </MenuItem>
+          </SubMenu> 
+        </Menu>
+        </SidebarContent>
+        <SidebarFooter>
+        <Menu iconShape="circle">
+            <MenuItem icon={<IoIcons.IoMdSettings />}>
+              Paramètres
+              <Link to="/settings" />
+            </MenuItem>
+          </Menu>
+        </SidebarFooter>
+      </ProSidebar>
+    </div>
+  );
 }
