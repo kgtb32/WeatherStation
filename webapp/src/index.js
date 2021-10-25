f940cd74105d406bbbf599206292e1949f7a9063
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-10-25 11:52:53 +0200

    Add SidebarVisibility feature

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
 
 import Sidebar from "./components/SideBar/SideBar";
+import TopBar from "./components/TopBar/TopBar";
+
 import temperatures from "./pages/Temperatures";
 import Pressure from "./pages/Pressure";
 import Humidity from "./pages/Humidity";
@@ -17,17 +19,32 @@ import "./i18n";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const breakPoint = 720;
+
 const Router = () => {
+	const [sideNavVisible, setSidenavVisible] = useState(true);
+	const sidebarVisible = () => {
+		return window.innerWidth <= breakPoint ? sideNavVisible : true;
+	};
+
+	const updateSidebarVisibility = () => {
+		setSidenavVisible(!sideNavVisible);
+	};
+
 	return (
 		<BrowserRouter>
 			<React.StrictMode>
 				<ContextProvider>
 					<div className="container_flex" style={{ height: "100vh" }}>
-						<Sidebar />
+						<Sidebar
+							sidebarVisible={sidebarVisible()}
+							setSidebarVisible={setSidenavVisible}
+						/>
 						<div
 							style={{ height: "100vh", background: "white", zIndex: 1024 }}
 							className="overflow-auto w-100"
 						>
+							<TopBar updateSidebarVisibility={updateSidebarVisibility} />
 							<Switch>
 								<Route path="/" component={Home} exact />
 								<Route path="/temperatures" component={temperatures} exact />
