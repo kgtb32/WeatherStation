7a5b079c7723e7c701fb8ad12ac1244e0f44949a
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-10-26 21:24:09 +0200

    Add basic/settings/set API

--- raspberry/hotspot-wifi/ServerMode/server/serverMode.py
+++ raspberry/hotspot-wifi/ServerMode/server/serverMode.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from utils.utils import write_file
+import json
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/basic/settings/set', methods = ['POST'])
+def basic_settings_set():
+    loc = request.form.get('location')
+    send_interval = request.form.get('send_interval')
+    if(loc == '' or send_interval == ''):
+        return jsonify(success=False)
+    else:
+        write_file("/tmp/config.json", json.dumps({"location": loc, "interval": send_interval}))
+        return jsonify(success=True)
     
