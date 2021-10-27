0b9d88d203fc192146e347e291a4da6ec6215ff5
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-10-27 10:13:41 +0200

    Add convert_nmcli_to_obj method in utils

--- raspberry/hotspot-wifi/ServerMode/server/utils/utils.py
+++ raspberry/hotspot-wifi/ServerMode/server/utils/utils.py
@@ -3,3 +3,24 @@ def write_file(file_to_write, data):
     f = open(file_to_write, "a")
     f.write(data)
     f.close()
+
+def convert_nmcli_to_obj(nmcli_result):
+    wifi_networks = []
+    network = {}
+    nb_items = 0
+    for i in range(0, len(nmcli_result)-1):
+        if("SSID:" in nmcli_result[i]):
+            network['ssid'] = nmcli_result[i].replace('SSID:', '').strip()
+            nb_items += 1
+        elif("SECURITY:" in nmcli_result[i]):
+            network['security'] = nmcli_result[i].replace('SECURITY:', '').strip()
+            nb_items += 1
+        elif("SIGNAL:" in nmcli_result[i]):
+            network['signal'] = nmcli_result[i].replace('SIGNAL:','').strip()
+            nb_items += 1
+        if(nb_items == 3):
+            wifi_networks.append(network)
+            network = {}
+            nb_items = 0
+        
+    return wifi_networks
