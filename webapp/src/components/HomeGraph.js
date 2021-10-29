e8274e4d69a0be405d43d853b500411dfbd4e71f
basile <basile-lequeux@hotmail.fr>
2021-10-29 11:42:02 +0200

    Display graphs on home page

---
+++ webapp/src/components/HomeGraph.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from "react";
+import ChartData from "./ChartData/ChartData";
+import { DataContext } from "../context";
+import CsvExport from "../components/DataExport/CsvExport";
+import { Dropdown } from "react-bootstrap";
+import dayjs from "dayjs";
+
+export default function HomeGraph({dataName}) {
+
+    const {
+        fetchDataRangeDate, 
+        dataWeather
+    } = DataContext()
+    
+    const [dataTemperature, setDataTemperature] = useState([]);
+    
+    useEffect(() => {
+        const last24h = dayjs().subtract(1, 'day').unix()
+        fetchDataRangeDate(last24h)
+    }, [])
+
+    useEffect(() => {
+        if (dataWeather) {
+            let prevState = []
+            if (dataName === 'temperature') {
+                dataWeather.map((e,i)=>
+                    prevState.push([e.date, e.temperature])
+                )
+            }
+            if (dataName === 'humidity') {
+                dataWeather.map((e,i)=>
+                    prevState.push([e.date, e.humidity])
+                )
+            }
+            if (dataName === 'pressure') {
+                dataWeather.map((e,i)=>
+                    prevState.push([e.date, e.pressure])
+                )
+            }
+            setDataTemperature(prevState)
+        }
+    }, [dataWeather])
+    
+    const data = 
+    {
+        label: dataName,
+        data: dataTemperature
+    }
+
+    return(
+        <>
+            <ChartData data={data}/>
+        </>
+    )
+
+}
--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import HomeGraph from "../components/HomeGraph";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Home() {
 
 	return (
-		<div className="container">
-			coucou
-		</div>
+		<>
+			<Container>
+				<Row>
+					<Col md={6}><HomeGraph dataName="temperature"/></Col>
+    				<Col md={6}><HomeGraph  dataName="pressure"/></Col>
+  				</Row>
+  				<Row>
+    				<Col md={{ span: 6, offset: 3 }}><HomeGraph  dataName="humidity"/></Col>
+  				</Row>
+			</Container>
+		</>
 	);
 }
