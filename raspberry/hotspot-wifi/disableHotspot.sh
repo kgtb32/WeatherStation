cf2840e31ab5c21ac51f17448dab566a81310bf0
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-11-01 17:07:55 +0100

    Updated enable/disable Hotspot scripts

--- raspberry/hotspot-wifi/disableHotspot.sh
+++ raspberry/hotspot-wifi/disableHotspot.sh
@@ -3,4 +3,6 @@
 sudo systemctl enable wpa_supplicant
 sudo systemctl start wpa_supplicant
 sleep 5
+sudo systemctl disable hostspot-wifi
+sudo systemctl stop hotspot-wifi
 sudo /etc/raspap/hostapd/servicestart.sh  --action stop
